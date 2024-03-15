c9244858553d3809737f8a33d074a012a3185811
Julio Duran <64277237+duranjulio2004@users.noreply.github.com>
2024-03-15 14:09:20 +0000

    Juego del dado

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -1,3 +1,5 @@
+import random
+
 def juego_del_dado():
     """
     Esta función tiene que pedirle al usuario que aprete enter para que lance un dado.
@@ -5,4 +7,26 @@ def juego_del_dado():
     Después el computador también tiene que lanzar un dado.
     El primero en sumar 30 puntos gana.
     """
-    pass
+    ptos_usuario = 0
+    ptos_cpu = 0
+    win = False
+    while win == False:
+        print("Presiona enter para tirar el dado")
+        enter = input()
+        
+        dado_usuario = random.randint(1,6)
+        print(f"Sacaste: {dado_usuario}")
+        ptos_usuario += dado_usuario
+
+        if ptos_usuario >= 30:
+            print("Ganaste!")
+            win = True
+            break
+
+        dado_cpu = random.randint(1,6)
+        print(f"Cpu saco: {dado_cpu}")
+        ptos_cpu += dado_cpu
+
+        if ptos_cpu >= 30:
+            print("Perdiste :(")
+            win = True
